00e25dc8df15fe6a0ff5b91d5b01824927aeaeba
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-16 15:25:17 +0530

    imported header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
 
-      <h1>Welcome to eCommerce Shopping Application</h1>
+      <Header />
+
+      <main>
+        <h1>Welcome to eCommerce Shopping Application</h1>
+      </main>
+
+      <Footer />      
 
     </>
   );
