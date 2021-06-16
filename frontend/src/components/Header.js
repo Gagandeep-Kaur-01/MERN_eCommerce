1ec368b2edc9b0c45f51e75519679f5a01f0900d
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-16 16:38:18 +0530

    added cdn

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,11 +4,11 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
     return (
         <header>
-          <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+          <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/"> eCommerce Shopping </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Brand href='/'> eCommerce Shopping </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ml-auto'>
                         <Nav.Link href='/cart'> Cart </Nav.Link>
                         <Nav.Link href='/login'> Sign In </Nav.Link>        
