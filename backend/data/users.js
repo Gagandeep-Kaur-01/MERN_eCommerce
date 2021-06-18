fa5916d5883ff9d33f743258ed35cf57fb8eac92
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-18 17:16:54 +0530

    created users

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: 'Simran',
+        email: 'simran@example.com',
+        password: xxxx,
+    },
+    {
+        name: 'Gagan',
+        email: 'gagan@example.com',
+        password: xxxx,
+    },
+]
