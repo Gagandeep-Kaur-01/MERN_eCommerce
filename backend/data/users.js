8d8c08941291420219da002095dd7a13fe152d7d
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-18 17:25:18 +0530

    hashed password

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true
     },
     {
         name: 'Simran',
         email: 'simran@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'Gagan',
         email: 'gagan@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
+
+export default users
