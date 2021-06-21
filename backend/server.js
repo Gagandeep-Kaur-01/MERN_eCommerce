da1db7c4bd32728bbe67d621f2136f303cca6a0a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-21 15:26:40 +0530

    created separate file for notFound and errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
@@ -26,20 +27,9 @@ app.use('/api/products', productRoutes)
 
 
 // For api which is not in actual route
-app.use((req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-})
+app.use(notFound)
 
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack:process.env.NODE_ENV === 'production' ? null : err.stack
-    })
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8080
 
