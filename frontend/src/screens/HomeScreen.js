3a9df0c74b8137a04eb1eb2322536edbeee18093
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-06-29 10:16:49 +0530

    working n bringing redux state into HomeScreen- useDispatch & useSelector, also changed server port

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import { lisrProducts, listProduct } from '../actions/productActions'
+import { listProducts } from '../actions/productActions'
 // import products from '../products'
 // import axios from 'axios'
 
@@ -11,11 +11,16 @@ import { lisrProducts, listProduct } from '../actions/productActions'
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
+
   // dispatching data using redux
   useEffect(() => {
-    dispatch(listProduct())    
+    dispatch(listProducts())    
     }, [dispatch]
   )
+
+  // const products = []
 
    // fetch data from backend
    //   const [products, setProducts] = useState([])
@@ -33,13 +38,19 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2> 
+        ) : error ? ( 
+          <h3>{error}</h3> 
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        )}      
     </>
   )
 }
