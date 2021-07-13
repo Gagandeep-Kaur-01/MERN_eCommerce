7519a628d4fe4273d06b0de6d7674ef0d52ad7d5
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-07-13 12:44:27 +0530

    creating 'Message & Loader components

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export const Loader = () => {
     return (
-        <div>
-            
-        </div>
+        <Spinner 
+            animation='border' 
+            role='status' 
+            style={{ 
+                width: '100px',
+                height: '100px', 
+                margin: 'auto', 
+                display: 'block' 
+            }}
+        >
+            <span class='sr-only'>Loading...</span>                        
+        </Spinner>
     )
 }
