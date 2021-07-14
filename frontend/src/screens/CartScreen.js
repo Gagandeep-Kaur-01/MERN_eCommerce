4c992baf62a806a651c7f8f50d3b48965fbe992b
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-07-14 18:52:45 +0530

    working on Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -13,7 +13,7 @@ const CartScreen = ( {match, location, history }) => {
 
     const dispatch = useDispatch()
 
-    const cart = useSelector(state => state.cart)
+    const cart = useSelector((state) => state.cart)
     const { cartItems } = cart
 
     
@@ -23,17 +23,22 @@ const CartScreen = ( {match, location, history }) => {
         }
     }, [dispatch, productId, qty])
 
+    const removeFromCartHandler = (id) => {
+        console.log('remove')
+    }
+
 
     return (
         <Row>
             <Col md={8}>
                 <h1>Shopping Cart</h1>
-                {cartItems.length === 0 ? 
-                <Message>
-                    Your cart is empty <Link to='/'>Go Back</Link>
-                </Message> : (
+                {cartItems.length === 0 ? (
+                    <Message>
+                        Your cart is empty <Link to='/'>Go Back</Link>
+                    </Message>
+                ) : (
                     <ListGroup variant='flush'>
-                        {cartItems.map(item => (
+                        {cartItems.map((item) => (
                             <ListGroup.Item key={item.product}>
                                 <Row>
                                     <Col md={2}>
@@ -46,7 +51,22 @@ const CartScreen = ( {match, location, history }) => {
                                         ${item.price}
                                     </Col>
                                     <Col md={2}>
-                                        
+                                        <Form.Control 
+                                            as='select' 
+                                            value={qty} 
+                                            onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                                         >
+                                            {[...Array(item.countInStock).keys()].map((x) => (
+                                              <option key={x+1} value={x + 1}>
+                                                {x + 1}
+                                              </option>
+                                            ))}
+                                        </Form.Control>
+                                    </Col>
+                                    <Col md={2}>
+                                        <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
